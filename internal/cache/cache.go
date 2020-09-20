8c9882700c07f4ba0ec76d4f1bc3d8060f2154eb
Levin Weinstein <weinsteinlevin@gmail.com>
2020-09-19 17:29:54 -0700

    [AddCandleStuffKindOf] Add Candle and Pair structs
    Added candle and pair structs.
    Also included an initializer for Pair and a factory for Candle

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
 import (
+	"strconv"
+
 	"github.com/google/wire"
 )
 
@@ -12,10 +14,72 @@ type (
 	Cache interface {
 	}
 
+	Candle struct {
+		open      float64
+		close     float64
+		high      float64
+		low       float64
+		timestamp string
+	}
+
+	Pair struct {
+		five    []*Candle // 3
+		fifteen []*Candle // 2
+		thirty  []*Candle // 2
+		hour    []*Candle // 1
+	}
+
 	CandlesCache struct {
-		value string
+		pairs map[string]Pair
 	}
 )
+
+/**
+ * NewCandle does stuff
+ */
+func NewCandle(open string, close string, high string, low string, timestamp string) (*Candle, error) {
+	output := &Candle{}
+
+	value, err := strconv.ParseFloat(open, 64)
+	if err != nil {
+		return nil, err
+	}
+	output.open = value
+
+	value, err = strconv.ParseFloat(close, 64)
+	if err != nil {
+		return nil, err
+	}
+	output.close = value
+
+	value, err = strconv.ParseFloat(high, 64)
+	if err != nil {
+		return nil, err
+	}
+	output.high = value
+
+	value, err = strconv.ParseFloat(low, 64)
+	if err != nil {
+		return nil, err
+	}
+	output.low = value
+
+	output.timestamp = timestamp
+	return output, nil
+
+}
+
+func InitializePair() *Pair {
+
+	output := &Pair{}
+
+	output.five = make([]*Candle, 3)
+	output.fifteen = make([]*Candle, 2)
+	output.thirty = make([]*Candle, 2)
+	output.hour = make([]*Candle, 2)
+
+	return output
+}
 
 func New() *CandlesCache {
 
