5479621fc3b127d93116031f6c4319b6591d9d20
Ethen <empociask@dons.usfca.edu>
2020-10-12 04:00:27 -0700

    [postgre] updated ddl

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE transactions(
-    trade_id UID DEFAULT NOT NULL,
-    time_stamp TIMESTAMP, 
+    trade_id UUID NOT NULL DEFAULT uuid_generate_v4 (),
+    time_stamp TIMESTAMP,
     pair VARCHAR(8),
     price NUMERIC,
     quantity NUMERIC,
