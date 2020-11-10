984666664dfb24b8d626f5e747e5812be92b1ad8
Adrian Smith <acsmith11@dons.usfca.edu>
2020-11-09 23:53:54 -0800

    Fixing side_car script

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ start-prod:
 	docker-compose -f docker-compose-prod.yaml up
 
 docker-run:
-	docker run -d ${BIN_NAME}
+	docker run --network="host" --log-opt max-size=10m --log-opt max-file=5 -d ${BIN_NAME}
 
 ecr-push-image:
 	docker push ${ECR_URI}/${BIN_NAME}
--- control_panel/driver.py
+++ control_panel/driver.py
@@ -5,21 +5,8 @@ import os
 import time
 
 
-def get_version() -> str:
-   
-    with open("version", "r") as f:
-        version = f.readlines()[0]
-    
-    return version
-
-def is_new_version(curr_version) -> bool:
-    if curr_version != get_version():
-        return True
-    
-    return False
-        
 def spinup():
-    os.system("make docker-run >> id.txt")
+    os.system("docker run --network=\"host\" --log-opt max-size=10m --log-opt max-file=5 --restart always -d tickers >> id.txt")
     
     with open("id.txt", "r") as fr:
         container: str = fr.readlines()[-1].replace("\n", "")
@@ -31,31 +18,26 @@ def start_db(container: str):
     os.system(f"bash switch_state.sh {container}")
 
 def destroy(container: str):
-    os.system(f"kill {container}")
+    os.system(f"docker kill {container}")
 
-def run(blue_container: str, green_container: str):
-    update_time: bool = False
-    curr_version = get_version()
+def run(blue_container, green_container):
+    update_time: bool = False 
     while True: 
-        if blue_container is None:
+        if blue_container == None:
             blue_container = spinup()
             start_db(blue_container)
 
-        if update_time is True:
+        if update_time:
             green_container = spinup()
             destroy(blue_container)
-            time.sleep(10)
             start_db(green_container)
             blue_container = green_container
             green_container = None
-            update_time = can_update =  False 
+            update_time = False
 
-        if is_new_version(curr_version):
+        if datetime.now().hour % 4 == 0 and datetime.now().minute == 0 and datetime.now().second <=1:
+
             update_time = True
-            curr_version = get_version()
-            print(curr_version)    
-
 
 if __name__ == '__main__':
     run(None, None)
-
