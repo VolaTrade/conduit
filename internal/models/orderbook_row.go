5fb3256bada11af1547b73f743374f90c199465d
kdcurrie <kdcurrie@dons.usfca.edu>
2021-01-14 16:18:37 -0800

    Changed how orderbookrow is stored, only converts to json.RawMessage at insertion to db, is there better way to do this?

--- internal/models/orderbook_row.go
+++ internal/models/orderbook_row.go
@@ -5,49 +5,22 @@ import (
 	"time"
 )
 
-type OrderBookRes struct {
-	Id   int        `json:"lastUpdateId"`
-	Bids [][]string `json:"bids"`
-	Asks [][]string `json:"asks"`
-}
-
 type OrderBookRow struct {
-	Id        int             `db:"id"`
-	Bids      json.RawMessage `db:"bids"`
-	Asks      json.RawMessage `db:"asks"`
-	Timestamp time.Time       `db:"timestamp"`
-	Pair      string          `db:"pair"`
-}
-
-func NewOrderBookRow(jsonResponse *OrderBookRes, pair string) (*OrderBookRow, error) {
-	bids, err := json.Marshal(jsonResponse.Bids)
-	if err != nil {
-		return nil, err
-	}
-
-	asks, err := json.Marshal(jsonResponse.Asks)
-	if err != nil {
-		return nil, err
-	}
-
-	return &OrderBookRow{
-		Id:        jsonResponse.Id,
-		Timestamp: time.Now(),
-		Pair:      pair,
-		Asks:      asks,
-		Bids:      bids,
-	}, nil
+	Id        int        `json:"lastUpdateId"`
+	Bids      [][]string `json:"bids"`
+	Asks      [][]string `json:"asks"`
+	Timestamp time.Time  `json:"timestamp"`
+	Pair      string     `json:"pair"`
 }
 
 func UnmarshalOrderBookJSON(message []byte, pair string) (*OrderBookRow, error) {
-	var jsonResponse OrderBookRes
+	var jsonResponse OrderBookRow
 	if err := json.Unmarshal(message, &jsonResponse); err != nil {
 		return nil, err
 	}
-	ob, err := NewOrderBookRow(&jsonResponse, pair)
-	if err != nil {
-		return nil, err
-	}
 
-	return ob, nil
+	jsonResponse.Timestamp = time.Now()
+	jsonResponse.Pair = pair
+
+	return &jsonResponse, nil
 }
--- internal/store/postgres/orderbook.go
+++ internal/store/postgres/orderbook.go
@@ -1,18 +1,54 @@
 package postgres
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/volatrade/conduit/internal/models"
 )
+
+//is there a way to insert the Bids and Asks into DB without storing as json.RawMessage?
+type DbOrderBookRow struct {
+	Id        int             `db:"id"`
+	Bids      json.RawMessage `db:"bids"`
+	Asks      json.RawMessage `db:"asks"`
+	Timestamp time.Time       `db:"timestamp"`
+	Pair      string          `db:"pair"`
+}
 
 const (
 	TRANSACTION_INSERTION_QUERY = `INSERT INTO transactions(trade_id, time_stamp, pair, price, quantity, is_maker) VALUES(:id, :timestamp, :pair, :price, :quant, :maker) ON CONFLICT DO NOTHING`
 	DEPTH_INSERTION_QUERY       = `INSERT INTO order_books (id, pair, time_stamp, bids, asks) VALUES (:id, :pair, :timestamp, :bids, :asks) ON CONFLICT DO NOTHING;`
 )
 
-func (postgres *DB) InsertOrderBookRow(obUpdate *models.OrderBookRow) error {
+func NewDBOrderBookRow(obRow *models.OrderBookRow) (*DbOrderBookRow, error) {
+	bids, err := json.Marshal(obRow.Bids)
+	if err != nil {
+		return nil, err
+	}
+
+	asks, err := json.Marshal(obRow.Asks)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DbOrderBookRow{
+		Id:        obRow.Id,
+		Bids:      bids,
+		Asks:      asks,
+		Timestamp: obRow.Timestamp,
+		Pair:      obRow.Pair,
+	}, nil
+}
+
+func (postgres *DB) InsertOrderBookRow(obRow *models.OrderBookRow) error {
 	stmt, err := postgres.DB.PrepareNamed(DEPTH_INSERTION_QUERY)
+	if err != nil {
+		return err
+	}
+
+	obUpdate, err := NewDBOrderBookRow(obRow)
 	if err != nil {
 		return err
 	}
@@ -48,7 +84,13 @@ func (postgres *DB) BulkInsertOrderBookRows(orderBookRows []*models.OrderBookRow
 		if orderBookRow == nil {
 			continue
 		}
-		result, err := stmt.Exec(orderBookRow)
+
+		obUpdate, err := NewDBOrderBookRow(orderBookRow)
+		if err != nil {
+			return err
+		}
+
+		result, err := stmt.Exec(obUpdate)
 
 		if err != nil {
 			postgres.logger.Errorw(err.Error(), "description", "executing query")
