9614a27547d3ac351e68f5c4f223a7b4b693c540
Ethen <empociask@dons.usfca.edu>
2021-04-26 18:33:07 -0700

    [STAGING_FIX] less verbose logging (#58)
    * [STAGING_FIX] less verbose logging
    
    * [STAGING_FIX] quick changes

--- internal/requests/cortex.go
+++ internal/requests/cortex.go
@@ -16,8 +16,9 @@ const (
 //PostOrderbookRow sends a POST request to Cortex to update it with the most recent orderbook data
 func (cr *ConduitRequests) PostOrderbookRowToCortex(orderbookRow *models.OrderBookRow) error {
 
-	postUrl := fmt.Sprintf("%s:%d/%s", cr.cfg.CortexUrl, cr.cfg.CortexPort, pedersonURL)
+	postUrl := fmt.Sprintf("%s:%d%s", cr.cfg.CortexUrl, cr.cfg.CortexPort, pedersonURL)
 
+	cr.logger.Infow("Sending request to Cortex", "url", postUrl)
 	data, err := json.Marshal(orderbookRow)
 	if err != nil {
 		return err
@@ -26,7 +27,7 @@ func (cr *ConduitRequests) PostOrderbookRowToCortex(orderbookRow *models.OrderBo
 	resp, err := http.Post(postUrl, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		cr.statz.Increment("cortex.errors", 1)
-		return fmt.Errorf("response: %+v, error: %s", resp.Status, err)
+		return fmt.Errorf("response error: %s", err.Error())
 	}
 
 	if err := resp.Body.Close(); err != nil {
